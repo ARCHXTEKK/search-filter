2f56f7b64dd4b027f926f10c7dd08dbf485bb05e
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-04 14:31:07 +0500

    added logic, some fixes in stylesheets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import JSONDATA from './DATA.json';
 import './index.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className="app">
-      <input type="text" placeholder="Search..." className="input"/>
-      {JSONDATA.map( (val, key) => {
+      <input type="text" placeholder="Search..." className="input" onChange={event => {setSearchTerm(event.target.value)}}/>
+      {JSONDATA.filter(val => {
+        if (searchTerm == "") {
+          return val;
+        } 
+        else if(val['first-name'].toLowerCase().includes(searchTerm.toLowerCase()) || val['last-name'].toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val;
+        }
+
+      }).map( (val, key) => {
         return (
-          <div>{val['first-name']} {val['last-name']}</div>
-        )   
-      } )}
+          <div>
+            <p className='user'>{val['first-name']} {val['last-name']}</p>
+          </div>
+        )})
+      }
     </div>
   );
 }
