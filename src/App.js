c58b3b49c3c94a092259cc0f73872e34d458afdb
ARCHXTEKK <slaughterer_archxtekk@mail.ru>
2023-06-04 14:04:44 +0500

    added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import JSONDATA from './DATA.json';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <input type="text" placeholder="Search..." className="input"/>
+      {JSONDATA.map( (val, key) => {
+        return (
+          <div>{val['first-name']} {val['last-name']}</div>
+        )   
+      } )}
     </div>
   );
 }
